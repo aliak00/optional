76329d86addeba781fb359b7e6942dbef1681330
Sebastiaan de Schaetzen <sebastiaan.de.schaetzen@gmail.com>
2020-09-18 19:39:06 +0200

    Improve compile error detection and output

--- source/optional/match.d
+++ source/optional/match.d
@@ -21,43 +21,47 @@ public template match(handlers...) if (handlers.length == 2) {
 	        static if (is(typeof(handlers[0](opt.front)))) {
 	            alias someHandler = handlers[0];
 	            alias noHandler = handlers[1];
+				return doMatch!(someHandler, noHandler, O, T)(opt);
 	        } else static if (is(typeof(handlers[0]()))) {
 	            alias someHandler = handlers[1];
 	            alias noHandler = handlers[0];
+				return doMatch!(someHandler, noHandler, O, T)(opt);
 	        } else {
 				// One of these two is causing a compile error.
 				// Let's call them so the compiler can show a proper error warning.
 				failOnCompileError!(handlers[0], T);
 				failOnCompileError!(handlers[1], T);
 			}
-
-	        alias SomeHandlerReturn = typeof(someHandler(T.init));
-	        alias NoHandlerReturn = typeof(noHandler());
-	        enum isVoidReturn = is(typeof(someHandler(T.init)) == void) || is(typeof(noHandler()) == void);
-
-	        static assert(
-	            is(SomeHandlerReturn == NoHandlerReturn) || isVoidReturn,
-	            "Expected two handlers to return same type, found type '" ~ SomeHandlerReturn.stringof ~ "' and type '" ~ NoHandlerReturn.stringof ~ "'",
-	        );
-
-	        if (opt.empty) {
-	            static if (isVoidReturn) {
-	                noHandler();
-	            } else {
-	                return noHandler();
-	            }
-	        } else {
-	            static if (isVoidReturn) {
-	                someHandler(opt.front);
-	            } else {
-	                return someHandler(opt.front);
-	            }
-	        }
 		} else static if (is(typeof(opt.value) == Optional!T, T)) {
 			return opt.valueMatch!handlers;
 		} else {
 			pragma(msg, "Type of: " ~ typeof(opt.value).stringof);
 			static assert(0, "Cannot match!() on a " ~ O.stringof);
+		}
+	}
+}
+
+private auto doMatch(alias someHandler, alias noHandler, O, T)(ref auto O opt) {
+	alias SomeHandlerReturn = typeof(someHandler(T.init));
+	alias NoHandlerReturn = typeof(noHandler());
+	enum isVoidReturn = is(typeof(someHandler(T.init)) == void) || is(typeof(noHandler()) == void);
+
+	static assert(
+		is(SomeHandlerReturn == NoHandlerReturn) || isVoidReturn,
+		"Expected two handlers to return same type, found type '" ~ SomeHandlerReturn.stringof ~ "' and type '" ~ NoHandlerReturn.stringof ~ "'",
+	);
+
+	if (opt.empty) {
+		static if (isVoidReturn) {
+			noHandler();
+		} else {
+			return noHandler();
+		}
+	} else {
+		static if (isVoidReturn) {
+			someHandler(opt.front);
+		} else {
+			return someHandler(opt.front);
 		}
 	}
 }
@@ -75,14 +79,8 @@ private template valueMatch(handlers...) if (handlers.length == 2) {
 Prints out the correct compile error if the handler cannot be compiled.
 */
 private void failOnCompileError(alias handler, T)() {
-	static if (!is(typeof(handlers[0](T.init))) || !is(typeof(handlers[1](T.init)))) {
+	static if (!is(typeof(handler(T.init))) && !is(typeof(handler()))) {
 		cast(void) handler(T.init);
-		static assert(true,
-			"\n============================================================\n" ~
-			"The above delegate or function pointer has a compiler error.\n" ~
-			"Fix it before fixing the errors below.\n" ~
-			"============================================================\n"
-		);
 	}
 }
 
