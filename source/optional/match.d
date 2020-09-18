29ae5d68c31fd1edbaed56a97d7a3ded413aa8af
Sebastiaan de Schaetzen <sebastiaan.de.schaetzen@gmail.com>
2020-09-18 19:33:01 +0200

    fix errors being hidden by match

--- source/optional/match.d
+++ source/optional/match.d
@@ -17,17 +17,23 @@ import optional.optional;
 public template match(handlers...) if (handlers.length == 2) {
 	auto match(O)(auto ref O opt) {
 		static if (is(O == Optional!T, T)) {
+			pragma(msg, typeof(handlers));
 	        static if (is(typeof(handlers[0](opt.front)))) {
 	            alias someHandler = handlers[0];
 	            alias noHandler = handlers[1];
-	        } else {
+	        } else static if (is(typeof(handlers[0]()))) {
 	            alias someHandler = handlers[1];
 	            alias noHandler = handlers[0];
-	        }
+	        } else {
+				// One of these two is causing a compile error.
+				// Let's call them so the compiler can show a proper error warning.
+				failOnCompileError!(handlers[0], T);
+				failOnCompileError!(handlers[1], T);
+			}
 
-	        alias SomeHandlerReturn = typeof(someHandler(opt.front));
+	        alias SomeHandlerReturn = typeof(someHandler(T.init));
 	        alias NoHandlerReturn = typeof(noHandler());
-	        enum isVoidReturn = is(SomeHandlerReturn == void) || is(NoHandlerReturn == void);
+	        enum isVoidReturn = is(typeof(someHandler(T.init)) == void) || is(typeof(noHandler()) == void);
 
 	        static assert(
 	            is(SomeHandlerReturn == NoHandlerReturn) || isVoidReturn,
@@ -56,9 +62,27 @@ public template match(handlers...) if (handlers.length == 2) {
 	}
 }
 
+/**
+Performs a match on the `value` property of a variable.
+*/
 private template valueMatch(handlers...) if (handlers.length == 2) {
 	auto valueMatch(O)(auto ref O opt) {
 		return opt.value.match!handlers;
+	}
+}
+
+/**
+Prints out the correct compile error if the handler cannot be compiled.
+*/
+private void failOnCompileError(alias handler, T)() {
+	static if (!is(typeof(handlers[0](T.init))) || !is(typeof(handlers[1](T.init)))) {
+		cast(void) handler(T.init);
+		static assert(true,
+			"\n============================================================\n" ~
+			"The above delegate or function pointer has a compiler error.\n" ~
+			"Fix it before fixing the errors below.\n" ~
+			"============================================================\n"
+		);
 	}
 }
 
